04da52e2e769ae64de0ff4158deb795ce1589ce4
Dave <dave@webdave.de>
2023-05-30 12:46:30 +0200

    init xs

---
+++ projects/ngxs-example/src/app/personal/personal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-personal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './personal.component.html',
+  styleUrls: ['./personal.component.scss']
+})
+export class PersonalComponent {
+
+}
