94eca595dc3f048812ebdea016b583481437460c
Dave <dave@webdave.de>
2023-05-30 12:46:37 +0200

    init rx

---
+++ projects/ngrx-example/src/app/address/address.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-address',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './address.component.html',
+  styleUrls: ['./address.component.scss']
+})
+export class AddressComponent {
+
+}
---
+++ projects/ngrx-example/src/app/skills/skills.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-skills',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss']
+})
+export class SkillsComponent {
+
+}
