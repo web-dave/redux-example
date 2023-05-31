60a762eb087eceb1cc6b2bc73b3b38368f2de2ba
Dave <dave@webdave.de>
2023-05-31 10:15:07 +0200

    [ngxs] final

--- projects/ngxs-example/src/app/header/header.component.ts
+++ projects/ngxs-example/src/app/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { of } from 'rxjs';
 import { RouterModule } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { PersonalState } from '../personal/store/personal.state';
+import { AddressState } from '../address/store/address.state';
 
 @Component({
   selector: 'app-header',
@@ -11,7 +14,7 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  // store = inject(Store<AppState>);
-  personalGroupIsValid$ = of(true); // this.store.select(selectPersonalGroupIsValid);
-  addressGroupIsValid$ = of(true); // this.store.select(selectAddressGroupIsValid);
+  store = inject(Store);
+  personalGroupIsValid$ = this.store.select(PersonalState.selectIsValid);
+  addressGroupIsValid$ = this.store.select(AddressState.selectIsValid);
 }
